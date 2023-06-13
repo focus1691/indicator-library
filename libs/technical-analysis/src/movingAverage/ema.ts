859cd9a40b9fef75b50acd36466090e5fee9dcb4
Joshua <mrjoshuaali@yahoo.co.uk>
2023-06-13 21:37:54 +0100

    EMA

---
+++ libs/technical-analysis/src/movingAverage/ema.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common'
+import { countDecimals, round } from '@utils/math'
+import { MA_Periods, IEmaOutcome } from './movingAverage.types'
+
+@Injectable()
+export class EmaIndicator {
+  private periods: MA_Periods[] = [MA_Periods.TEN, MA_Periods.TWENTY, MA_Periods.FIFTY, MA_Periods.ONE_HUNDRED, MA_Periods.TWO_HUNDRED]
+  public static calculateEMA(data, period: MA_Periods): number[] {
+    const numDecimals: number = data.reduce((highestNumDecimals, ema) => {
+      const numDecimals: number = countDecimals(Number(ema))
+      return numDecimals > highestNumDecimals ? numDecimals : highestNumDecimals
+    }, 0)
+    const emas: number[] = [Number(data.shift())]
+    const smoothingFactor: number = 2 / (period + 1)
+
+    for (let i = 0; i < data.length; i++) {
+      const prevEma: number = emas[emas.length - 1]
+      const value: number = Number(data[i])
+      const ema: number = smoothingFactor * (value - prevEma) + prevEma
+      const roundedEma: number = round(ema, numDecimals)
+      emas.push(roundedEma)
+    }
+    return emas
+  }
+  calculateEmas(data: number[], periods: number[] = this.periods): IEmaOutcome {
+    const emaOutcome: IEmaOutcome = {} as IEmaOutcome
+
+    for (let i = 0; i < periods.length; i++) {
+      emaOutcome[periods[i]] = EmaIndicator.calculateEMA(data, periods[i])?.pop()
+    }
+    return emaOutcome
+  }
+}
